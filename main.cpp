6bf3a9a0e3b66291ef5d8946e6ff2fdcd0b83e06
Hugo Henrique Teixeira Zamith <hhzamith@gmail.com>
2025-05-05 12:35:05 +0100

    first commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-#include "irc.cpp"
+#include "irc.hpp"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if(argc != 3)
+	{
+		std::cerr << "Usage: " << argv[0] << " <server> <port>" << std::endl;
+		return 1;
+	}
+}
