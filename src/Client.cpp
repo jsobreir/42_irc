e40ccfc55fb9e3d22d99ce544cc4b071d4e96955
Hugo Henrique Teixeira Zamith <hhzamith@gmail.com>
2025-05-06 18:19:53 +0100

    PASS fix

--- src/Client.cpp
+++ src/Client.cpp
@@ -29,8 +29,12 @@ Client::~Client() {
 
 }
 
-void Client::setPasswd(std::string passwd) {
-    _password = passwd;
+void Client::setPasswd(std::string passwd)
+{
+    if (passwd.length() > 5)
+        _password = passwd.substr(5);
+    else
+        _password = "";
     _received_passwd = true;
 	std::cout << "Client " << _fd << " passwd is: " << _password << std::endl;
 }
